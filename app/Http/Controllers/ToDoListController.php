5ad8875313fdd272d59f32418be3982c70c6b57f
Moran Quesnel <moques@gmail.com>
2020-10-15 13:20:15 +0200

    Initial commit

---
+++ app/Http/Controllers/ToDoListController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\TodoList;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class ToDoListController extends Controller
+{
+    public function index()
+    {
+        $user = DB::table('todo_lists')->get();
+        return response()->json($user);
+    }
+
+    public function create(Request $request)
+    {
+
+        $this->validate($request, [
+            'description' => 'required',
+        ]);
+
+        $description = $request->input('description');
+        DB::table('todo_lists')->insertGetId(
+            [
+                'description' => $request->input('description'),
+                'created_at' => now()
+            ]
+        );
+
+        return response()->json($description,201);
+    }
+}
